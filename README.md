dca86232bb75b6b0bffc678c143939d6ff257d1c
João Vitor <joaovitordasilvafreitas@gmail.com>
2024-07-23 12:24:22 -0300

    Correção de erros no git, Adição do botão para remover todas as tasks

---
+++ README.md
@@ -0,0 +1,5 @@
+Colocando em prática os conhecimentos alguns conhecimentos adquiridos ao desenvolver da jornada no curso Alura.
+
+Manipulação de elementos DOM.
+CRUD
+Local Storage
---
+++ script.js
@@ -0,0 +1,69 @@
+const formularioAdicionarTarefa = document.querySelector('.container_adiciona-task-input');
+const botaoAdicionarTarefa = document.querySelector('.container_adiciona-task-botao');
+const botaoRemoverTodas = document.querySelector('#botao_remover_todos');
+const listaCompleta = document.querySelector('.lista_tasks');
+
+let listaDeItens = [];
+
+function adicionarNovaTarefa() {
+
+    if (formularioAdicionarTarefa.value === '') {
+        alert('O campo está vazio !!!')
+    } else {
+        listaDeItens.push({
+            tarefa: formularioAdicionarTarefa.value,
+            concluida: false,
+        });
+        mostrarTarefa();
+    }
+
+}
+
+function mostrarTarefa() {
+    let novaLi = '';
+    
+    if (Array.isArray(listaDeItens) && listaDeItens.length >= 0) {
+
+        listaDeItens.forEach((item, posicao) => {
+            novaLi = novaLi + `
+        <li class="linha_task ${item.concluida && "finalizada"}">
+        <img class="img_task img-checked" src="./imagens/checked.png" alt="check-tarefa"
+        onclick="concluirTarefa(${posicao})">
+        <p class="item_task-nome-tarefa">${item.tarefa}</p>
+        <img class="img_task img-delete" src="./imagens/trash.png" alt="deletar-tarefa" onclick="deletarTarefa(${posicao})">
+        </li>
+        `
+        });
+    }else {
+        console.error("A lista está vazia")
+    }
+
+    listaCompleta.innerHTML = novaLi;
+
+    localStorage.setItem('lista', JSON.stringify(listaDeItens));
+}
+
+function concluirTarefa(posicao) {
+    listaDeItens[posicao].concluida = !listaDeItens[posicao].concluida;
+    mostrarTarefa();
+}
+
+function deletarTarefa(posicao) {
+    listaDeItens.splice(posicao, 1);
+    mostrarTarefa();
+}
+
+function deletarTudo(posicao) {
+    listaDeItens.splice(0, 25);
+    mostrarTarefa();
+}
+
+function recarregarTarefas() {
+    const tarefasLocalStorage = localStorage.getItem('lista');
+    listaDeItens = JSON.parse(tarefasLocalStorage);
+    mostrarTarefa();
+}
+
+recarregarTarefas();
+botaoAdicionarTarefa.addEventListener('click', adicionarNovaTarefa);
+botaoRemoverTodas.addEventListener('click', deletarTudo);
